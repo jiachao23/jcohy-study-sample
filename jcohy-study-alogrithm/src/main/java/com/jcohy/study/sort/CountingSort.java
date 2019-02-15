f7131fd0e9fcb8a5866a0b672ecf04b73f1827bc
jiachao <jia_chao23@126.com>
2019-02-15 14:55:48 +0800

    add sort

--- jcohy-study-alogrithm/src/main/java/com/jcohy/study/sort/CountingSort.java
+++ jcohy-study-alogrithm/src/main/java/com/jcohy/study/sort/CountingSort.java
@@ -10,40 +10,40 @@ import java.util.Arrays;
 public class CountingSort {
 
     public static void main(String[] args) {
-        int[] data = {9,5,-1,8,5,7,3,-3,1,3};
+        int[] data = {9, 5, -1, 8, 5, 7, 3, -3, 1, 3};
         System.out.println("排序之前：\n" + java.util.Arrays.toString(data));
         countingSort(data);
         System.out.println("排序之后：\n" + java.util.Arrays.toString(data));
     }
 
-    public static void countingSort(int[] data){
-        if(data.length == 0){
+    public static void countingSort(int[] data) {
+        if (data.length == 0) {
             return;
         }
         //找出待排序的数组中最大和最小的元素；
-        int max  = data[0];
+        int max = data[0];
         int min = data[0];
-        for(int i= 0;i < data.length ;i++){
-            if(data[i] > max){
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > max) {
                 max = data[i];
             }
-            if(data[i] < min){
+            if (data[i] < min) {
                 min = data[i];
             }
         }
         int bias = 0 - min;
-        int[] bucket = new int[max - min +1];
-        Arrays.fill(bucket,0);
-        for(int i = 0 ; i< data.length;i++){
-            bucket[data[i]+bias]++;
+        int[] bucket = new int[max - min + 1];
+        Arrays.fill(bucket, 0);
+        for (int i = 0; i < data.length; i++) {
+            bucket[data[i] + bias]++;
         }
-        int index=0 ,i = 0;
-        while (index < data.length){
-            if(bucket[i] != 0){
+        int index = 0, i = 0;
+        while (index < data.length) {
+            if (bucket[i] != 0) {
                 data[index] = i - bias;
                 bucket[i]--;
                 index++;
-            }else{
+            } else {
                 i++;
             }
         }
